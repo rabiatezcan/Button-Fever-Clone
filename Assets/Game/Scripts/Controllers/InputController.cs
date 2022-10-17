5d2f39cfd573aa6976a08f02d8558a19f82a92e8
rabiatezcan <rabiatezcan@hotmail.com>
2022-10-17 15:47:57 +0300

    folder  changes

---
+++ Assets/Game/Scripts/Controllers/InputController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : Controller
+{
+    [SerializeField] private Camera _cameraTransform;
+
+    private InputHandler _inputHandler;
+    private ISelectableObject _currentObject;
+
+    #region Core
+    public override void Initialize(GameManager gameManager)
+    {
+        _inputHandler = new InputHandler();
+    }
+
+    public override void StartGame()
+    {
+        ListenInputs();
+    }
+
+    public override void Reload()
+    {
+    }
+
+    public override void GameSuccess()
+    {
+        StopListenInputs();
+        _inputHandler.RemoveInputs();
+    }
+
+    #endregion
+    private void ListenInputs()
+    {
+        _inputHandler.OnMouseButtonDown += Select;
+        _inputHandler.OnMouseButton += Drag;
+        _inputHandler.OnMouseButtonUp += Drop;
+    }
+    private void StopListenInputs()
+    {
+        _inputHandler.OnMouseButtonDown -= Select;
+        _inputHandler.OnMouseButton -= Drag;
+        _inputHandler.OnMouseButtonUp -= Drop;
+    }
+
+    public void Select()
+    {
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, 100f, LayerMask.GetMask("SelectableObject")))
+        {
+            if (hit.collider != null)
+            {
+                _currentObject = hit.transform.GetComponentInParent<ISelectableObject>();
+
+                _currentObject.Select();
+            }
+        }
+    }
+
+    public void Drag(Vector2 deltaPos)
+    {
+        if (_currentObject != null)
+            _currentObject.Drag(deltaPos);
+    }
+
+    public void Drop()
+    {
+        if (_currentObject != null)
+        {
+            _currentObject.Drop();
+            _currentObject = null;
+        }
+
+    }
+
+    private void Update()
+    {
+        _inputHandler.Update();
+    }
+
+    public override void GameFail()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Game/Scripts/Core/Level/Level.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public void Initialize()
+    {
+    }
+    public void Build()
+    {
+    }
+
+    public void Remove()
+    {
+    }
+}
