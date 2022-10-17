0ff836cb40f5cd95627299d8a177c99b3f935a24
rabiatezcan <rabiatezcan@hotmail.com>
2022-10-17 18:39:34 +0300

    added button value txt

--- Assets/Game/Scripts/Button/ButtonBody.cs
+++ Assets/Game/Scripts/Button/ButtonBody.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class ButtonBody : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> _bodies; 
+    [SerializeField] private List<GameObject> _bodies;
+    [SerializeField] private TextMeshPro _valueTxt;
     public void Initialize(GameEnums.ButtonTypes bodyType)
+    {
+        SetBody(bodyType);
+    }
+    private void SetBody(GameEnums.ButtonTypes bodyType)
     {
         HideAllBodies();
         ShowBody(bodyType);
+        SetValueTxt(bodyType);
     }
 
+    private void SetValueTxt(GameEnums.ButtonTypes value)
+    {
+        _valueTxt.text = "+" + GetBodyValue(value);
+    }
     private void ShowBody(GameEnums.ButtonTypes bodyType)
     {
         _bodies[((int)bodyType)].SetActive(true);
@@ -19,5 +31,10 @@ public class ButtonBody : MonoBehaviour
     private void HideAllBodies()
     {
         _bodies.ForEach(body => body.SetActive(false));
+    }
+
+    private int GetBodyValue(GameEnums.ButtonTypes value)
+    {
+        return (((int)value) * 2) + 1;
     }
 }
